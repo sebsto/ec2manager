fbaa6c28c70fbf2454c5be38b0ca98a8041251a6
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2024-02-20 14:12:56 +0100

    move build number increase after build

--- ci_actions/03_build.sh
+++ ci_actions/03_build.sh
@@ -10,14 +10,15 @@ pushd $CODE_DIR
 CONFIGURATION="Release"
 
 # Increase Build Number
-# https://rderik.com/blog/automating-build-and-testflight-upload-for-simple-ios-apps/
+# Does not work here because Xcode generates the Info.plist file dynamically
 
+# https://rderik.com/blog/automating-build-and-testflight-upload-for-simple-ios-apps/
 #BUILD_NUMBER=`date +%Y%m%d%H%M%S`
 #echo "Updated build number is " $BUILD_NUMBER
 #plutil -replace CFBundleVersion -string $BUILD_NUMBER "./EC2Manager/Info.plist"
 
 # https://developer.apple.com/library/archive/qa/qa1827/_index.html
-agvtool next-version -all
+# agvtool next-version -all
 
 security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
 
@@ -28,5 +29,10 @@ xcodebuild clean archive                    \
            -derivedDataPath "${BUILD_PATH}" \
            -configuration "$CONFIGURATION"  \
            -destination platform="iOS Simulator",name="${PHONE_MODEL}",OS=${IOS_VERSION} | $BREW_PATH/xcbeautify
+
+# Update build number in the archive. Each build must have a unique number before uploading to AppStore connect
+BUILD_NUMBER=`date +%Y%m%d%H%M%S`
+echo "Updated build number is " $BUILD_NUMBER
+plutil -replace ApplicationProperties.CFBundleVersion -string $BUILD_NUMBER "build-release/EC2Manager.xcarchive/Info.plist"
 
 popd
